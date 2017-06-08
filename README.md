2fe3d7a9cd484928c71e6bbc9f0b3df535c830d4
Kitchu0401 <kitchu@naver.com>
2017-06-08 17:29:53 +0900

    README.md 및 크롤링 배치 코드 리팩토링 및 주석 보완

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# lazecrew-batch-service
+# Event-notifier
+
+**키워드 기반**으로 모임에 대한 정보를 **텔레그램 챗봇**을 통해 알려주는 application입니다.
+
+# Event-source
+
+`2017-06-08` 기준 아래의 사이트에서 10여분 단위의 웹-크롤링을 통해 정보를 추출하여 제공하고 있습니다.
+
+추후 다른 소스를 알게되면 추가 할 수 있겠죠. 나쁜사람 아닙니다, 크롤링을 막지 말아주세요. ヾ(;ﾟДﾟ;)ｼ
+
+- [onoffmix.com](http://onoffmix.com/)
+
+# Installation
+
+## npm
+
+서비스 셋업을 위해 다음의 기본적인 과정을 수행합니다.
+
+``` sh
+git clone <REPOSITORY_URL>
+
+npm install
+```
+
+## mongodb
+
+기본 포트의 **Mongodb**를 데이터베이스로 사용합니다.
+
+설치는 [공식 도큐먼트](https://www.mongodb.com/download-center)를 참조해주세요.
+
+## telegram-bot-webhook
+
+telegram bot 메시지 수신을 통한 상태 업데이트를 위해 **WebHook**을 사용합니다.
+
+`2017-06-08` 기준 webHook 설정을 위해:
+
+- **self-signed certification**을 사용합니다. [공식 가이드 문서]()를 참조해주세요.
+
+- telegram rest api setWebHook을 통해 webHook을 설정한 후, 별도의 https 요청을 받아 처리하고 있습니다.
+
+  ``` sh
+  curl <TELEGRAM_SETWEBHOOK_REST_API_URL> -F <CERTIFICATE_KEY_FILE_PATH> <MY_WEBHOOK_URL>
+  ```
+
+추후 도메인 전체를 위한 인증서를 적용하고, 서비스 구동시 동적으로 webHook을 설정하도록 변경 할 계획입니다.
--- batchs/onoffmixEventNotifier.js
+++ batchs/onoffmixEventNotifier.js
@@ -15,6 +15,9 @@ module.exports = (function () {
 
   console.log(`[${_config.jobName}] TelegramBot has been initialized with token: ${_config.telegramBotToken}`)
   
+  /**
+   * 모임정보를 추출하여 텔레그램 메시지를 발송한다.
+   */
   function crawl () {
     request(_config.url, (error, response, body) => {
       if ( error ) {
@@ -22,59 +25,58 @@ module.exports = (function () {
         return
       }
 
-      // Request 호출시간화초기화
+      // Request 호출시간 초기화
       let taskTs = ts()
 
-      // init cheerio context
-      let promises = $('ul.todayEvent:not(.alwaysShow)', '#content', body).map(function () {
-        let event = {
-          index: $(this).attr('_idx'),
-          thumbnail: $(this).find('li.eventThumbnail > a > img').attr('src'),
-          link: $(this).find('li.eventTitle > a').attr('href'),
-          title: $(this).find('li.eventTitle > a').attr('title'),
-          extractTime: taskTs
-        }
+      // 크롤링으로 추출한 이벤트 객체들을 각각
+      // db existance check > save Promise로 래핑하여 반환한다.
+      let promises = $('ul.todayEvent:not(.alwaysShow)', '#content', body)
+        .map(function () {
+          // cheerio context 내 'this'는 selector에서 선택된 가상 DOM element
+          let event = {
+            index: $(this).attr('_idx'),
+            thumbnail: $(this).find('li.eventThumbnail > a > img').attr('src'),
+            link: $(this).find('li.eventTitle > a').attr('href'),
+            title: $(this).find('li.eventTitle > a').attr('title'),
+            extractTime: taskTs
+          }
 
-        // title 항목이 존재하지 않는 경우 저장하지 않는다.
-        return event.title ? event : null
-      })
-      .get()
-      .map((event) => {
-        // 새롭게 발견한 이벤트에 대해 Inserted object를, 그 외에는 null을 반환한다.
-        return Event
-          .findOne({ index: event.index })
-          .then((found) => { return !found ? new Event(event).save().then((saved) => { return saved }) : null })
-      })
+          // title 항목이 존재하지 않는 경우 저장하지 않는다.
+          return event.title ? event : null
+        })
+        .get()
+        .map((event) => {
+          // 새롭게 발견한 이벤트에 대해 save Promise를
+          // 그 외에는 null을 반환한다.
+          return Event
+            .findOne({ index: event.index })
+            .then((found) => { return !found ? new Event(event).save().then((saved) => { return saved }) : null })
+        })
 
       Promise
         .all(promises)
         .then((resultList) => {
-          // 새롭게 발견한 이벤트에 대해 작업 수행 결과를 출력하고, 후처리 로직을 호출한다.
+          // 새롭게 발견한 이벤트에 대해
           let newEventList = resultList.filter((result) => { return result !== null })
+
+          // 작업 수행 결과를 출력하고
           let newEventListStr = newEventList.reduce((str, event) => { return str + `\n[${_config.jobName}][${taskTs}] - ${event.title}` }, '')
-          
           console.log(`[${_config.jobName}][${taskTs}] Request done with ${newEventList.length} events.${newEventListStr}`)
+
+          // 후처리 로직을 호출한다.
           if ( newEventList.length > 0 ) {
             notifyUser(newEventList)
           }
         })
     })
   }
-  
-  function loadUserInfo () {
-    return User
-      .find()
-      .then((userList) => {
-        // keyword를 등록한 사용자에 대해서만 알린다.
-        userList = userList.filter((user) => { return user.tags.length > 0 })
 
-        console.log(`[${_config.jobName}][${ts()}] Loading userInfo done with ${userList.length} users.`)
-        return userList
-      })
-  }
-
+  /**
+   * 신규 모임 정보를 받아 대상 사용자들에게 텔레그램 메시지를 발송한다.
+   * @param {Array} eventList - 신규 모임 목록
+   */
   function notifyUser (eventList) {
-    // 등록된 Telegram 사용자 정보를 조회한다.
+    // Telegram 사용자 정보를 조회한다.
     loadUserInfo()
       .then((subscriberList) => {
         subscriberList.forEach((subscriber) => {
@@ -82,6 +84,7 @@ module.exports = (function () {
           let subscribedEventList = eventList.filter((event) => { return subscriber.regexp.test(event.title) })
 
           if ( subscribedEventList.length > 0 ) {
+            // 사용자에게 발송될 메시지 본문을 생성한다.
             let messageHeader = `[${ts()}]\n새로운 모임을 발견했습니다.`
             let messageBody = subscribedEventList.map((event) => {
                 return `${event.title}\nlink: ${event.link}`
@@ -92,22 +95,49 @@ module.exports = (function () {
               .replace(/\*/g, '') // preventing error
               .replace(subscriber.regexp, '*$1*')
             
-            // 텔레그램 봇을 통해 메시지를 발송한다.
+            // 텔레그램 봇을 통해 모임 안내 메시지를 발송한다.
             bot.sendMessage(subscriber.id, message, { parse_mode: 'Markdown' })
           }
         })
       })
       .catch((error) => { console.error(error) })
   }
+  
+  /**
+   * 메시지 발송을 위한 사용자 목록 조회 promise를 반환한다.
+   * @return {Promise} 사용자 목록 조회 promise
+   */
+  function loadUserInfo () {
+    return User
+      // 활성화되었으며,
+      // keyword를 등록한 사용자만 조회한다.
+      .find({ active: 1, 'tags.0': { $exists: true } })
+      .then((userList) => {
+        console.log(`[${_config.jobName}][${ts()}] Loading userInfo done with ${userList.length} users.`)
+        return userList
+      })
+  }
 
+  /**
+   * 작업 간격으로 활용할 millisecond 단위 무작위 int 값을 반환한다.
+   * @return {number} 작업 간격
+   */
   function getRandomInterval () {
     return parseInt(Math.ceil(Math.random() * 4) + 8) * 1000 * 60
   }
 
+  /**
+   * 로깅을 위한 Timestamp 문자열을 반환한다.
+   * @return {string} Timestamp 문자열
+   */
   function ts () {
     return moment().format('YYYY-MM-DD HH:mm:ss')
   }
 
+  /**
+   * 수행할 작업을 인자로 넘겨받아 무작위 간격으로 반복수행한다.
+   * @param {function} task - 반복수행할 독립 프로세스
+   */
   function startInterval (task) {
     task()
     setTimeout(() => {
@@ -122,4 +152,3 @@ module.exports = (function () {
   }
 
 })()
-
